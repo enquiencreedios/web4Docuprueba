3301553cb53bf2e2c84a264efbed250836c67977
GonzaM <enquiencreedios@gmail.com>
2026-04-05 09:18:39 -0300

    cambiosconfig

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,16 +15,16 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://enquiencreedios.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/web4Docuprueba/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'enquiencreedios', // Usually your GitHub org/user name.
+  projectName: 'web4Docuprueba', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
   onBrokenLinks: 'throw',
 
   // Even if you don't use internationalization, you can use this field to set
